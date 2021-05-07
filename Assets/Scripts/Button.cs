c461d4bb9eb985840bc50ff0bebe2a344e369439
Alesandra <alesandrabbell@gmail.com>
2021-05-06 20:35:37 -0500

    Stats Update Now + Fixed Particles
    Stats in the righthand corner update now, UI particles have been fixed

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] AudioClip _hoverSound;
+    [SerializeField] int _stat;
+    
+
+    private void OnMouseEnter()
+    {
+        //Debug.Log("Choice Button.");
+        AudioHelper.PlayClip2D(_hoverSound, 7);
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField] AudioClip _backgroundMusic;
+
+    //Player Stats
+    [SerializeField] int _smarts = 5;
+    [SerializeField] Text _smartsText;
+    [SerializeField] int _boldness = 4;
+    [SerializeField] Text _boldnessText;
+    [SerializeField] int _creativity = 6;
+    [SerializeField] Text _creativityText;
+    [SerializeField] int _charm = 4;
+    [SerializeField] Text _charmText;
+    [SerializeField] int _fun = 4;
+    [SerializeField] Text _funText;
+    [SerializeField] int _money = 8;
+    [SerializeField] Text _moneyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateStats();
+
+        if(_backgroundMusic != null)
+        {
+            AudioManager.Instance.PlaySong(_backgroundMusic);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateStats();
+    }
+
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    void UpdateStats()
+    {
+        _smartsText.text = _smarts.ToString();
+        _boldnessText.text = _boldness.ToString();
+        _creativityText.text = _creativity.ToString();
+        _charmText.text = _charm.ToString();
+        _funText.text = _fun.ToString();
+        _moneyText.text = _money.ToString();
+    }
+
+    public void ChangeCreativity(int value)
+    {
+        _creativity += value;
+    }
+
+    public void ChangeCharm(int value)
+    {
+        _charm += value;
+    }
+
+
+}
